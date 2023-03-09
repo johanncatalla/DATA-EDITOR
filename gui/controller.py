e9b7d917a1acc113cd665968a7637936782fb016
Johann Sebastian Catalla <101336933+johanncatalla@users.noreply.github.com>
2023-03-09 12:14:44 +0800

    Add files via upload

--- gui/controller.py
+++ gui/controller.py
@@ -8,20 +8,26 @@ import re
 import os
 
 class Controller():
+    # Controller object that will bind the view and model to create main app
     def __init__(self):
+        # root container
         self.root = tk.Tk()
-        self.model = Model()
-
-        self.view = View(self.root, self) 
         self.root.geometry("800x800")
         self.root.title("New File")
-
+        # model object
+        self.model = Model()
+        # view object
+        self.view = View(self.root, self) 
+        # bind to keyboard which triggers function that concatenates string to the string storage 
         self.view.viewPanel.txt_editor.bind('<KeyRelease>', self.on_key_release)
+        # binds the keyboard shortcuts for the CRUD
         self.view.viewPanel.txt_editor.bind("<KeyPress>", self.shortcut)
 
+        # flag to check if a file is opened
         global open_status_name
         self.open_status_name = False
 
+        # flag to check is there is text selected
         global selected
         self.selected = False
 
@@ -29,7 +35,7 @@ class Controller():
         self.menu_bar = tk.Menu(self.root)
         self.root.config(menu=self.menu_bar)
 
-        # Adding FILE menu containing that has the functions "Open File...", "New Text File", "Save", "Save as...", and "Delete file"
+        # FILE menu containing "Open File...", "New Text File", "Save", "Save as...", and "Delete file"
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
         self.file_menu.add_command(label="Open File...", command=self.open_text_file)
         # adds separator to organize the file menu relative to the functionality of its commands
@@ -41,11 +47,11 @@ class Controller():
         self.file_menu.add_separator()
         self.file_menu.add_command(label="Delete File", command=self.delete_file)     
         
-        # Add second menu called ACTION menu
+        # ACTION menu
         self.action_menu = tk.Menu(self.menu_bar, tearoff=0)
         self.action_menu.add_command(label="Close window", command=self.on_closing)
 
-        # Add third menu called EDIT menu containing the functions "cut", "copy", and "paste"
+        # EDIT menu containing "cut", "copy", and "paste"
         self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
         self.edit_menu.add_command(label="Cut", command=lambda: self.cut_text(False))
         self.edit_menu.add_separator()
@@ -53,35 +59,38 @@ class Controller():
         self.edit_menu.add_separator()
         self.edit_menu.add_command(label="Paste", command=lambda: self.paste_text(False))
         
-
-        # Add cascade and set labels for menus
+        # add cascade and labels for menus
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)
         self.menu_bar.add_cascade(label="Actions", menu=self.action_menu)
         self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
 
+        # adds a protocol when closing tab to trigger yes or no prompt
         self.root.protocol(
             "WM_DELETE_WINDOW",
             self.on_closing
         )
 
     def run(self):
+        """runs the program"""
         self.root.mainloop()
 
     def switch_window(self):
+        """Open the CSV Viewer"""
         view_csv = Application()
         view_csv.run()
     
     def search_txt(self):
+        """Search functionality of Text Editor"""
         # getting the text from the Text editor
         text_editor_input = self.view.viewPanel.txt_editor.get("1.0", tk.END)
         entry_input = self.view.viewPanel.entry.get()
         option_value = self.view.viewPanel.value_inside.get()
 
+        # call search_sentence function to find list of all sentence matches
         lst_searches = self.model.search_sentence(text_editor_input, entry_input, option_value)
+        # Transforming the list of results into string with lines in between for readability
         string_searches = "\n\n".join(lst_searches)
 
-        # Transforming the list of results into string with lines in between for readability
-        
         # Inserting the string of results to the text editor in the display frame
         sentences = f"\nMatches:\n\n{string_searches}\n\n------END OF RESULTS------\n\n"
         self.view.viewPanel.update_display(sentences)
@@ -102,9 +111,11 @@ class Controller():
         self.view.viewPanel.display_text.insert('1.0', f"Sentence matches: {self.num_matches}\n")
     
     def destroy(self):
+        """Clears the search results"""
         self.view.viewPanel.display_text.delete('1.0', tk.END)
 
     def shortcut(self, event):
+        """CRUD shortcuts for the text editor"""
         # checks if the user presses certain key combinations
         # "Ctrl + s" saves the file
         if event.state == 4 and event.keysym == "s": 
@@ -120,10 +131,12 @@ class Controller():
             self.delete_file()
 
     def on_key_release(self, event):
+        """Inserts the inputted string to variable per key release"""
         self.model.text = self.view.viewPanel.txt_editor.get('1.0', tk.END)
 
-    # create functions for edit menu options 
-    def cut_text(self,e): # cuts text
+    # Functions for edit menu
+    def cut_text(self,e):
+        """cuts the text"""
         global selected
         if self.view.viewPanel.txt_editor.selection_get():
             # Grabs the selected text from the text editor
@@ -131,12 +144,14 @@ class Controller():
             # Deletes the selected text from the text editor
             self.view.viewPanel.txt_editor.delete("sel.first", "sel.last")
 
-    def copy_text(self,e): # copies text
+    def copy_text(self,e):
+        """copies the text"""
         if self.view.viewPanel.txt_editor.selection_get():
             # Grabs the selected text from the text editor
             self.selected = self.view.viewPanel.txt_editor.selection_get()
 
-    def paste_text(self,e): # pastes text
+    def paste_text(self,e):
+        """pastes the text"""
         if self.selected:
             # Finds the position of the text cursor
             self.position = self.view.viewPanel.txt_editor.index(tk.INSERT)
@@ -144,12 +159,13 @@ class Controller():
             self.view.viewPanel.txt_editor.insert(self.position, self.selected)
 
     def open_text_file(self):
+        """opens file and inserts it to text editor"""
         f = fd.askopenfilename(
             initialdir="D:/Downloads/", 
             title='Open File', 
             filetypes=(('.txt files', '*.txt'), ('HTML Files', '*.html'),('Python Files', '*.py'), ('All Files', '*.*'))
         )
-       
+        
         if f:
             # Make filename global to access it later
             global open_status_name
@@ -160,22 +176,25 @@ class Controller():
             self.view.viewPanel.status_bar.config(text=f"{f}       ")            
             self.root.title(f"{extract_filename}")
 
+            # update text editor
             self.model.open(f)
             self.view.viewPanel.update(self.model.text)
 
     def new_file(self):
+        """create new file"""
         # Deletes previous text
         self.view.viewPanel.update()
         # Updates the title and status bar
         self.root.title('New File')
         self.view.viewPanel.status_bar.config(text="New File       ")
 
-        # Since the new file is still not saved in the directory, sets the text editor flag to False
-        # so that when we click the "Delete File" option in the "File Menu", the previous file will not be deleted
+        # Since the new file is still not saved in the directory, sets text editor flag to False
+        # so that when we click the "Delete File", the previous file will not be deleted
         global open_status_name
         self.open_status_name = False   
 
     def save_file(self):
+        """saves file"""
         # checks the text editor flag if the file exists in the directory
         if self.open_status_name:
             # Save the file
@@ -189,19 +208,22 @@ class Controller():
             self.save_as_file() 
     
     def save_as_file(self):
+        """saves as file if file does not exist"""
         text_file = fd.asksaveasfilename(
             defaultextension=".*", 
             initialdir="D:/Downloads/", 
             title="Save File as", 
             filetypes=(('.txt files', '*.txt'), ('HTML Files', '*.html'),('Python Files', '*.py'), ('All Files', '*.*'))
         )
+        print(text_file)
         # checks if the user opened a file in the file dialog
         if text_file:
             # Updade Status Bars
             name = text_file
             self.view.viewPanel.status_bar.config(text=f"Saved: {name}       ")
-            name = name.replace("D:/Downloads/", "")
-            self.root.title(f"{name}")
+            # gets the file name and inserts it to title
+            extract_filename = re.search(r"[^/\\]+$", text_file).group(0)
+            self.root.title(f"{extract_filename}")
             
             # Save the file
             self.model.save(text_file)
@@ -211,6 +233,7 @@ class Controller():
         self.open_status_name = text_file
 
     def save_export(self):
+        """exports the search results into .txt file"""
         # Saves the file as... using the "asksaveasfilename" function from the filedialog module
         # Filetypes can be .txt, .html, .py, or All Files
         text_file = fd.asksaveasfilename(
@@ -232,6 +255,7 @@ class Controller():
             pass
 
     def delete_file(self):
+        """triggers when deleting the file, then triggers the prompt"""
         # check if there is a file opened or the file exists in the directory
         if self.open_status_name:
             # checks if the path exists
@@ -247,7 +271,7 @@ class Controller():
     
     # Deletes the file from the directory
     def on_deletion(self):
-        
+        """triggers yes or no prompt when deleting file"""
         # regex that takes tha file name from the directory
         extract_filename = re.search(r"[^/\\]+$", self.open_status_name).group(0)
         #regex that takes the directory from the file name
@@ -263,6 +287,7 @@ class Controller():
             messagebox.showinfo(title="Message", message=f"Successfuly deleted \"{extract_filename}\" from {directory_path}.")
     
     def on_closing(self):
+        """triggers yes or no prompt when closing the window"""
         # checks if the user intends to close the window
         if messagebox.askyesno(title="Close?", message=f"Do you really want to close Text Editor?"):
             self.root.destroy()
