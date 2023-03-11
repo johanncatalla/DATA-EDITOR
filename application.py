8259dafeb6d1e752237a61ca3ddd83d9b3467a1e
Johann Sebastian Catalla <101336933+johanncatalla@users.noreply.github.com>
2023-03-11 10:23:11 +0800

    Add files via upload

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
-from gui.controller import Controller        
+from gui.controller import Controller
+import tkinter as tk       
 
 if __name__ == '__main__':
-    app = Controller()
-    app.run()
+    
+    main = Controller()
+    main.run()
